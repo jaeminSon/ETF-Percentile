3ab03340804be469f1d3b37d8919aab16b1ed4ee
jaeminSon <woalsdnd@gmail.com>
2025-04-06 20:23:31 +0900

    [refactor] remove date from get query

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -5,8 +5,6 @@ from datetime import datetime
 import numpy as np
 from flask import Blueprint, request, jsonify
 
-from gauger.statistics import density_function, distribution
-
 from .models import Stock
 
 
@@ -26,38 +24,34 @@ def percentile(values: List[float], index_target: int) -> float:
     return np.mean(np.array(values) < target_value) * 100
 
 
-def find_index_target(dates: List[datetime.date], date: datetime.date) -> int:
-    for i in range(len(dates) - 1, -1, -1):
-        if dates[i] == date:
-            return i
-
-
-def price_ratio_and_percentiles(
-    d: Dict[str, List[object]], date: str, window: int
-) -> Tuple[List[float], List[float], List[float], List[float]]:
-    date = datetime.strptime(date, "%Y-%m-%d").date()
-
-    index_target = find_index_target(d["date"], date)
-    price_ratio_percentile = percentile(d[f"price_ratio_{window}ma"], index_target)
-    volume_percentile = percentile(d["volume"], index_target)
+def extract_data(d: Dict[str, List[object]], window: int) -> Dict[str, object]:
+    ticker = d["ticker"][0]
+    all_dates = [e.strftime("%Y-%m-%d") for e in d["date"]]
+    price_ratio = d[f"price_ratio_{window}ma"]
+    price_ratio_percentile = percentile(price_ratio, len(all_dates) - 1)
+    volume_percentile = percentile(d["volume"], len(all_dates) - 1)
 
     return {
-        "price_ratio": d[f"price_ratio_{window}ma"],
-        "price_ratio_percent": price_ratio_percentile,
-        "volume_percent": volume_percentile,
+        "ticker": ticker,
+        "date": all_dates,
+        "price_ratio": price_ratio,
+        "price_ratio_percentile": price_ratio_percentile,
+        "volume_percentile": volume_percentile,
     }
 
 
 @main.route("/stocks", methods=["GET"])
 def get_stocks():
     ticker = request.args.get("ticker", type=str)
-    date = request.args.get("date", type=str)
     window = request.args.get("window", type=int)
 
-    res_data = Stock.query.filter(Stock.date <= date, Stock.ticker == ticker).all()
+    res_data = (
+        Stock.query.filter(Stock.ticker == ticker).order_by(Stock.date.asc()).all()
+    )
     d = res2dict(res_data)
 
     # TODO return if values in db
 
-    res = price_ratio_and_percentiles(d, date, window)
+    res = extract_data(d, window)
+    res["window"] = window
     return jsonify(res)
