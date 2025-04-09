60599b5f61ed7ebf567229ada6b7609a4412fef8
jaeminSon <woalsdnd@gmail.com>
2025-04-10 00:11:50 +0900

    [bug fix] volume -> price

--- backend/gauger/statistics.py
+++ backend/gauger/statistics.py
@@ -116,24 +116,26 @@ def stock_data(
     tickers: List[str],
     start_date: str,
     end_date: str = None,
+    return_moving_average=False,
 ):
     df = download(tickers, start_date, end_date)
 
     data = {}
     for ticker in tickers:
         df_by_window = [
-            df["Volume"][ticker].to_frame("price"),
+            df["Close"][ticker].to_frame("price"),
             df["Volume"][ticker].to_frame("volume"),
         ]
-        for i, window in enumerate([20, 50, 100, 200]):
-            price_ratio, price_ma = divide_by_rolling_ma(
-                df["Close"][ticker], window, True
-            )
-            price_ratio = price_ratio.to_frame(f"price_ratio_{window}ma")
-            price_ma = price_ma.to_frame(f"price_{window}ma")
+        if return_moving_average:
+            for i, window in enumerate([20, 50, 100, 200]):
+                price_ratio, price_ma = divide_by_rolling_ma(
+                    df["Close"][ticker], window, True
+                )
+                price_ratio = price_ratio.to_frame(f"price_ratio_{window}ma")
+                price_ma = price_ma.to_frame(f"price_{window}ma")
 
-            merged_df = merge_dataframes([price_ratio, price_ma])
-            df_by_window.append(merged_df)
+                merged_df = merge_dataframes([price_ratio, price_ma])
+                df_by_window.append(merged_df)
 
         data[ticker] = merge_dataframes(df_by_window)
 
@@ -143,9 +145,10 @@ def stock_data(
 def all_stock_data(
     start_date: str,
     end_date: str = None,
+    return_moving_average=True,
 ):
     tickers = all_tickers()
-    return stock_data(tickers, start_date, end_date)
+    return stock_data(tickers, start_date, end_date, return_moving_average)
 
 
 def distribution(
