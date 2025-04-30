5e3612ca9f5cec0f32ebc281ceada20b5c89390a
jaeminSon <woalsdnd@gmail.com>
2025-05-01 00:23:39 +0900

    modify path

--- frontend/api.ts
+++ frontend/api.ts
@@ -1,6 +1,5 @@
 export const fetchChartData = async (ticker: string, window: number) => {
-  const BASE_URL = "http://jaemin-lab.ddns.net:8000";
-  // const BASE_URL = "http://10.0.2.2:8000";
+  const BASE_URL = "https://jaemin-lab.ddns.net/api/";
   const url = `${BASE_URL}/stocks?ticker=${ticker}&window=${window}`;
   const res = await fetch(url);
   const data = await res.json();
