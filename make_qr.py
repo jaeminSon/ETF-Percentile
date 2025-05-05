c55295124b7c02e79c9a8d4acb9676ff6be5d352
jaeminSon <woalsdnd@gmail.com>
2025-05-05 14:55:08 +0900

    [dev] add qr

---
+++ make_qr.py
@@ -0,0 +1,9 @@
+import qrcode
+
+url = "https://jaemin-lab.ddns.net/"
+qr = qrcode.make(url)
+qr.save("url_qr.png")
+
+# play_store_url = "https://play.google.com/store/apps/details?id=com.example.myapp"
+# qr = qrcode.make(play_store_url)
+# qr.save("android_app_qr.png")
