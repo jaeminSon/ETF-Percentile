c593df6f1303e52528c687bc69d2d76de64b0904
jaeminSon <woalsdnd@gmail.com>
2025-05-06 20:57:35 +0900

    [rename] repo

--- backend/app/log.py
+++ backend/app/log.py
@@ -1,10 +1,13 @@
+import os
 import logging
+
+home_log = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("/Users/sonjaemin/StockGauger/backend.log"),
+        logging.FileHandler(os.path.join(home_log, "backend.log")),
         logging.StreamHandler(),
     ],
 )
