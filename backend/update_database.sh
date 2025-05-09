ce24b859f05cffcac33b245d843af0fc4ae91a25
jaeminSon <woalsdnd@gmail.com>
2025-05-09 17:04:45 +0900

    [refactor] specify python bin explicitly

--- backend/update_database.sh
+++ backend/update_database.sh
@@ -1,4 +1,12 @@
+# update daily
 curl -X POST localhost:8000/update-database
 
+# compute percentile for caching
+for t in SPY SPXL QQQ TQQQ SOXX SOXL; do
+   for w in 20 50 100 200; do
+	curl "localhost:8000/percentile?ticker=${t}&window=${w}"
+   done
+done
+
 cd /Users/sonjaemin/ETF-Percentile/backend
-python3 -m gauger.plot
+/Library/Frameworks/Python.framework/Versions/3.11/bin/python3.11 -m gauger.plot
