43d9390270a047daf8466a091b346b61a91b47de
jaeminSon <woalsdnd@gmail.com>
2025-06-11 21:17:03 +0900

    add android app qr code

--- make_qr.py
+++ make_qr.py
@@ -4,6 +4,6 @@ url = "https://jaemin-lab.ddns.net/"
 qr = qrcode.make(url)
 qr.save("url_qr.png")
 
-# play_store_url = "https://play.google.com/store/apps/details?id=com.example.myapp"
-# qr = qrcode.make(play_store_url)
-# qr.save("android_app_qr.png")
+play_store_url = "https://play.google.com/store/apps/details?id=com.jaeminson.etfpercentile"
+qr = qrcode.make(play_store_url)
+qr.save("android_app_qr.png")
