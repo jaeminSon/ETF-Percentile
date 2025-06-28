e4f83448d3943daab8cfb1deb63115c2c143ff0d
jaeminSon <woalsdnd@gmail.com>
2025-06-28 14:46:51 +0900

    [refactor] black and ruff

--- make_qr.py
+++ make_qr.py
@@ -4,6 +4,8 @@ url = "https://jaemin-lab.ddns.net/"
 qr = qrcode.make(url)
 qr.save("url_qr.png")
 
-play_store_url = "https://play.google.com/store/apps/details?id=com.jaeminson.etfpercentile"
+play_store_url = (
+    "https://play.google.com/store/apps/details?id=com.jaeminson.etfpercentile"
+)
 qr = qrcode.make(play_store_url)
 qr.save("android_app_qr.png")
