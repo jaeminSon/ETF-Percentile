191e566d6590ad8ed4d00c44aa4ccba814e3cb97
jaeminSon <woalsdnd@gmail.com>
2025-06-29 10:02:45 +0900

    update tickers

--- backend/update_database.sh
+++ backend/update_database.sh
@@ -4,11 +4,12 @@ while ! curl -X POST -s -o /dev/null -w "%{http_code}" localhost:8000/update-dat
 done
 
 # compute percentile for caching
-for t in SPY SPXL QQQ TQQQ IWM TNA DIA DDM TLT TMF IEF GLD SLV SMH SOXL BITX VNQ IBB KBE XLY XLP XLE XLF FAS XLV XLI XLB XME XOP OIH XRT XTL XLK XLU DBC PFF ITB; do
+for t in SPY SPXL QQQ TQQQ IWM TNA DIA DDM TLT TMF IEF GLD SLV SMH SOXL BITX XLF FAS AAPL AAPU AMZN AMZU COIN CONL META FBL GOOGL GGLL MSFT MSFU NVDA NVDL TSLA TSLL; do
    for w in 20 50 100 200; do
 	curl "localhost:8000/percentile?ticker=${t}&window=${w}"
    done
 done
+
 
 cd /Users/sonjaemin/ETF-Percentile/backend
 /Library/Frameworks/Python.framework/Versions/3.11/bin/python3.11 -m gauger.plot
