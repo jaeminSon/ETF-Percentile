351c511e617c34ebd2c6cb967c0362a69085c37e
jaeminSon <woalsdnd@gmail.com>
2025-08-24 20:19:20 +0900

    test all pages

--- load_testing.sh
+++ load_testing.sh
@@ -2,4 +2,8 @@
 # ab -n 5000 -c 100 "https://jaemin-lab.ddns.net/api//percentile?ticker=META&window=100"
 
 # bulk query
-ab -n 1000 -c 100 "https://jaemin-lab.ddns.net/api/page=category=index"
+ab -n 500 -c 100 "https://jaemin-lab.ddns.net/api/page=category=index"
+ab -n 500 -c 100 "https://jaemin-lab.ddns.net/api/page=category=tech"
+ab -n 500 -c 100 "https://jaemin-lab.ddns.net/api/page=category=asset"
+ab -n 500 -c 100 "https://jaemin-lab.ddns.net/api/page=category=global"
+ab -n 500 -c 100 "https://jaemin-lab.ddns.net/api/page=category=sector"
